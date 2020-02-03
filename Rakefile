be38ff9ed26cf2207d74bfb90c90873c3d554668
Sarah Condon <s.j.condon01@gmail.com>
2020-02-03 14:40:56 -0600

    Done.

--- Rakefile
+++ Rakefile
@@ -4,5 +4,3 @@
 require File.expand_path('../config/application', __FILE__)
 
 Rails.application.load_tasks
-
-module TempFixForRakeLastComment def last_comment last_description
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,22 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
     @posts = Post.all
   end
-
+  
   def new
+
   end
 
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
   end
-  
+
+  private 
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
+  end
 end
